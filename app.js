d99e7dbb83fcd747445e71c019971e57a1e9e86b
JasonNham <jason123nham@gmail.com>
2021-04-29 11:56:22 -0400

    My website commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// ===== Menu Show =====
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  /*Active link*/
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) =>
+  n.addEventListener("click", linkAction)
+);
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
